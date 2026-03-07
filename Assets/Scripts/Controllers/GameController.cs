4591e7d5891f7d75aeaf705854f168c517c5cce9
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2026-03-07 15:26:45 -0700

    Adding stat tracking

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : State
 {
+    public const int NUM_DAYS = 5;
+    public const float DAY_LENGTH_SECONDS = 5f * 60f;
+    public int currentDay = 0;
+    public float elapsed = 0.0f;
+
+    public Dictionary<string, float> stats = new();
+
     public GameController(MainController mainController, StateMachine stateMachine)
         : base(mainController, stateMachine) { }
 
@@ -16,6 +24,20 @@ public class GameController : State
     {
         base.Tick();
 
+        elapsed += Time.deltaTime;
+        if (elapsed >= DAY_LENGTH_SECONDS)
+        {
+            elapsed = 0f;
+            currentDay++;
+            Debug.Log($"Day {currentDay} starting!");
+
+            if (currentDay >= NUM_DAYS)
+            {
+                stateMachine.GotoState(mainController.endScreenController);
+                return;
+            }
+        }
+
         for (int i = 0; i < spriteGrid.sprites.Length; i++)
         {
             if (i >= mainController.PlotItems.Length)
@@ -28,12 +50,21 @@ public class GameController : State
 
             if (plot.IsFullyGrown())
             {
-                plot.Harvest(this);
+                float yield = plot.Harvest(this);
+                if (!stats.ContainsKey(plot.Being.IconName))
+                {
+                    stats[plot.Being.IconName] = 0f;
+                }
+                stats[plot.Being.IconName] += yield;
             }
 
             SpriteGridCell spriteGridCell = spriteGrid.sprites[i];
             spriteGridCell.RenderPlotItem(plot);
         }
+
+        // TODO Show day progress ui here
+
+        // TODO Update board ui here
     }
 
     public override void HandleButtonSelect(int index)
